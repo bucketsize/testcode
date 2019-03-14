420e11216d381ecf93bf636c84ee4b4faa81bb37
Jaya Balan A <bucket.size@gmail.com>
2019-03-14 10:37:10 +0530

    via mitmproxy; curl k equiv

--- hsfun/src/Twit.hs
+++ hsfun/src/Twit.hs
@@ -10,24 +10,41 @@ import Data.Time.Clock (UTCTime)
 import Data.Text (Text)
 import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as L8
-
+import qualified Data.ByteString.Char8 as C8
+import System.Environment
+import Network.Connection
 
 --authHost = "localhost:8000"
 --apiHost = "localhost:8000"
 authHost = "api.twitter.com"
-apiHost = "api.twitter.com"
+apiHost  = "api.twitter.com"
 
-twitOAuth :: OAuth
-twitOAuth = newOAuth
-  { oauthServerName      = authHost
-  , oauthConsumerKey     = ""
-  , oauthConsumerSecret  = ""
-  }
+noSSLVerifyManager :: IO Manager
+noSSLVerifyManager =
+  let tlsSettings = TLSSettingsSimple {
+      settingDisableCertificateValidation = True
+      , settingDisableSession=False
+      , settingUseServerName=True
+      }
+   in newManager $ mkManagerSettings tlsSettings Nothing
 
-twitCred :: Credential
-twitCred = newCredential
-  ""
-  ""
+twitOAuth :: IO OAuth
+twitOAuth = do
+  key <- getEnv "TwitConKey"
+  sec <- getEnv "TwitConSec"
+  return newOAuth
+    { oauthServerName      = authHost
+    , oauthConsumerKey     = C8.pack key
+    , oauthConsumerSecret  = C8.pack sec
+    }
+
+twitCred :: IO Credential
+twitCred = do
+  tok <- getEnv "TwitAccTok"
+  sec <- getEnv "TwitAccSec"
+  return $ newCredential
+    (C8.pack tok)
+    (C8.pack sec)
 
 data Tweet = Tweet
   { text :: !Text
@@ -37,9 +54,12 @@ instance FromJSON Tweet
 instance ToJSON Tweet
 
 timeline name = do
-    req <- parseUrlThrow $ "https://"++ apiHost ++"/1.1/statuses/user_timeline.json?screen_name=" ++ name
-    signedreq <- signOAuth twitOAuth twitCred req
-    manager <- newManager tlsManagerSettings
+    req  <- parseUrlThrow $ "https://"++ apiHost ++"/1.1/statuses/user_timeline.json?screen_name=" ++ name
+    auth <- twitOAuth
+    cred <- twitCred
+    signedreq <- signOAuth auth cred req
+    -- manager <- newManager tlsManagerSettings
+    manager <- noSSLVerifyManager
     res <- httpLbs signedreq manager
     L8.putStrLn $ responseBody res
     --return $ eitherDecode $ responseBody res
