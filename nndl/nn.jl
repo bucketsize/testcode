9378433f0da36db65f2e9e0620d08c006c8bddaa
JB <bucket.size@gmail.com>
2021-08-13 01:05:15 +0530

    incorrect impl?

--- nndl/nn.jl
+++ nndl/nn.jl
@@ -1,4 +1,4 @@
-struct NeuralNet
+mutable struct NeuralNet
     L :: Vector{Int64}
     W :: Vector{Matrix{Float32}}
     B :: Vector{Vector{Float32}}
@@ -26,9 +26,17 @@ function createNN(L::Vector{Int64})::NeuralNet
     NeuralNet(L, W, B)
 end
 function descNN(nn::NeuralNet)
-    println("layers:", nn.L)
-    println("weights:", nn.W)
-    println("bias:", nn.B)
+    println("=========== NN =============")
+    println("layers:  ", nn.L)
+    println("weights: ", nn.W)
+    println("bias:    ", nn.B)
+end
+function descTP(C, dC, dW, dB)
+    println("------------ tmp -----------")
+    println("C:  ", C)
+    println("dC: ", dC)
+    println("dW: ", dW)
+    println("dB: ", dB)
 end
 function feedForward(nn::NeuralNet,
                      xs::Vector{Float32})::Vector{Float32}
@@ -38,36 +46,71 @@ function feedForward(nn::NeuralNet,
     end
     y
 end
-function costi(nn::NeuralNet,
-               xs::Vector{Float32},
-               ys::Vector{Float32},
-               n::Int64)::Vector{Float32}
-    local as = feedForward(nn, xs)
-    local c = (1/2*n) * broadcast(sq, (ys - as))
-    #println("costi:", typeof(c), size(c), c)
-    c
-end
 function cost(nn::NeuralNet,
               xss::Vector{Vector{Float32}},
-              yss::Vector{Vector{Float32}})::Vector{Float32}
-    local cs = zeros(Float32, length(yss))
+              yss::Vector{Vector{Float32}})::Float32
+    local c = Float32(0)
+    local n = length(yss)
     for (xs, ys) in zip(xss, yss)
-        cs += costi(nn, xs, ys, length(xss))
+        local as = feedForward(nn, xs)
+        local cx = sum(broadcast(sq, (ys - as)))
+        c += cx
     end
-    cs
+    c / (2*n)
+end
+function gradientDescent(nn::NeuralNet,
+                         xss::Vector{Vector{Float32}},
+                         yss::Vector{Vector{Float32}},
+                         lR::Float32,
+                         N::Int64)::Float32
+    descNN(nn)
+    local C  = cost(nn, xss, yss)
+    local dW = lR*nn.W
+    local dB = lR*nn.B
+    nn.W += dW
+    nn.B += dB
+
+    for i in 1:N
+        descNN(nn)
+        local C1 = cost(nn, xss, yss)
+        local dC = C1 - C
+
+        divDeltaCby(x) = (dC/x)
+        local dW1 = []
+        for idW in dW
+            local grCw = broadcast(divDeltaCby, idW)
+            push!(dW1, - lR*grCw)
+        end
+        dW = dW1
+
+        local dB1 = []
+        for idB in dB
+            local grCb = broadcast(divDeltaCby, idB)
+            push!(dB1, - lR*grCb)
+        end
+        dB = dB1
+
+        nn.W += dW
+        nn.B += dB
+        C = C1
+
+        descTP(C, dC, dW, dB)
+    end
+    return C
 end
 
+trX =
+    [
+        Float32[0.1, 0.8, 0.7],
+        Float32[0.2, 0.5, 0.6]
+    ]
+trY =
+    [
+        Float32[0.8, 0.21],
+        Float32[0.7, 0.12]
+    ]
 
 nn = createNN([3, 5, 2])
-c = cost(nn,
-         [
-             Float32[0.1, 0.8, 0.7],
-             Float32[0.2, 0.5, 0.6]
-         ],
-         [
-             Float32[0.8, 0],
-             Float32[0.7, 0]
-         ]
-         )
+fc = gradientDescent(nn, trX, trY, Float32(0.01), 10)
 
-println(c)
+println(fc)
